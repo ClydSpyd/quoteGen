387953e14793e34b6c6cfc6ad8e205fd86ded6fd
ClydSpyd <47127754+ClydSpyd@users.noreply.github.com>
2019-03-14 12:35:49 +0100

    complimentFiles

---
+++ compliment/compliment.js
@@ -0,0 +1,102 @@
+
+//structure: [start] [superlative], most [adjective] [noun][ending]
+
+
+var start = ['This creature before me could be the', 'My darling, you are the', 'Might this be the']
+
+var superlative = ['sweetest','fairest','loveliest','rarest','daintiest']
+
+var adjective = ['precious','darling', 'delicate', 'celestial','artful','silken','heavenly','rosy-cheeked','fair-faced',]
+
+var noun = ['cuckoo-bud','true-penny','goddess','flower','nymph']
+
+var end = ['ever I hath lain eyes upon', "in all of God's realm", 'in the heavens as well as on Earth','that ever hath graced mine eye','to ever walk this mortal realm']
+
+var complimentOutput = document.getElementById('complimentOutput');
+
+var output='';
+var i = '';
+function gen(arr){
+    return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function generate() {
+complimentOutput.innerHTML='';
+i=0;
+output = gen(start) + ' ' + gen(superlative) +', most ' + gen(adjective) + ' ' + gen(noun) +' '+ gen(end);
+   if(output.charAt(1)==='i'){
+       output+='?';
+       console.log('yo');
+   } else{
+       output+='.';
+   }
+console.log(output);
+}  
+    
+
+function typing() {
+    if (i < output.length) {
+        complimentOutput.innerHTML += output.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+document.getElementById('go').addEventListener('click',function(){
+
+    generate();
+    typing();
+});
+
+
+/*deployment n main page
+
+var start = ['This creature before me could be the', 'My darling, you are the', 'Might this be the']
+
+var superlative = ['sweetest', 'fairest', 'loveliest', 'rarest', 'daintiest']
+
+var adjective = ['precious', 'darling', 'delicate', 'celestial', 'artful', 'silken', 'heavenly', 'rosy-cheeked', 'fair-faced',]
+
+var noun = ['cuckoo-bud', 'true-penny', 'goddess', 'flower', 'nymph']
+
+var end = ['ever I did lay eyes upon', "in all of God's realm", 'in the heavens as well as on Earth', 'that ever hath graced mine eye', 'to ever walk this mortal realm']
+
+var complimentOutput = document.getElementById('complimentOutput');
+
+function gen(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateComp() {
+    var output = gen(start) + ' ' + gen(superlative) + ', most ' + gen(adjective) + ' ' + gen(noun) + ' ' + gen(end);
+    if (output.charAt(1) === 'i') {
+        output += '?';
+        console.log('yo');
+    } else {
+        output += '.';
+    }
+    return output;
+}
+
+//trigger
+function clear(){msg2.innerHTML = ''};
+hit.addEventListener('click', function () {
+    output = generateComp();
+    var i = 0, text;
+    $hit.animate({ opacity: 0 },500);
+
+    function visibility() {
+        $hit.animate({ opacity: 1 },1000);
+    }
+    msg.innerHTML = '';
+    function typing() {
+        if (i < output.length) {
+            document.getElementById('msg').innerHTML += output.charAt(i);
+            i++;
+            setTimeout(typing, 50);
+        }
+    }
+    typing();
+    setTimeout(visibility, 3000);
+    function change(){ hit.value = 'again'}
+    setTimeout(change,1000)
+})*/
